c6d171af93230a8518ec50bc7db8bdce2dcd7950
theDuck2322 <theduck2322@gmail.com>
2023-12-15 06:39:20 +0200

    more asteroids to main page added

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,24 +1,28 @@
 const asteroidCount = 25; // Adjust as needed
 
-function createAsteroid(container) {
-    const asteroid = document.createElement('div');
-    asteroid.className = 'asteroid';
+const asteroidImages = ['/Resources/asteroid1.png', '/Resources/asteroid2.png'];
 
-    // Adjust the initial vertical position (top) as needed
-    asteroid.style.top = `${Math.random() * -10}px`;
+    function createAsteroid(container) {
+        const asteroid = document.createElement('div');
+        asteroid.className = 'asteroid';
 
-    asteroid.style.left = `${Math.random() * window.innerWidth}px`;
+        // Adjust the initial vertical position (top) as needed
+        asteroid.style.top = `${Math.random() * -10}px`;
 
-    // Adjust the size of the asteroid randomly
-    const asteroidSize = Math.floor(Math.random() * (50 - 20 + 1)) + 20; // Adjust the size range as needed
-    asteroid.style.width = `${asteroidSize}px`;
-    asteroid.style.height = `${asteroidSize}px`;
+        asteroid.style.left = `${Math.random() * window.innerWidth}px`;
 
-    container.appendChild(asteroid);
-    animateAsteroid(asteroid, container);
-}
+        // Randomly choose an image path
+        const randomImage = asteroidImages[Math.floor(Math.random() * asteroidImages.length)];
+        asteroid.style.backgroundImage = `url('${randomImage}')`;
 
-// Rest of the code remains the same
+        // Adjust the size of the asteroid randomly
+        const asteroidSize = Math.floor(Math.random() * (50 - 20 + 1)) + 20; // Adjust the size range as needed
+        asteroid.style.width = `${asteroidSize}px`;
+        asteroid.style.height = `${asteroidSize}px`;
+
+        container.appendChild(asteroid);
+        animateAsteroid(asteroid, container);
+    }
 
 
 function animateAsteroid(asteroid, container) {
